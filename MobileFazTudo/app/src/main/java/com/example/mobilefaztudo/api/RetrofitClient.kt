565476f0e242e12b2a6dad6d064aed5766d558e5
cavalcantibeatriz <beatriz.cavalcanti@sptech.school>
2024-05-12 14:35:45 -0300

    Correção login e cadastro[adição  de headers na requisição]

--- MobileFazTudo/app/src/main/java/com/example/mobilefaztudo/api/RetrofitClient.kt
+++ MobileFazTudo/app/src/main/java/com/example/mobilefaztudo/api/RetrofitClient.kt
@@ -1,17 +1,35 @@
 package com.example.mobilefaztudo.api
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    //Insira seu ip fixo aqui e mude também no arquivo res/xml/network_security_config.xml
+    private const val BASE_URL = "http://192.168.0.124:8080/"
+    private const val CONNECT_TIMEOUT = 30
+    private const val CONTENT_TYPE_JSON = "application/json"
 
-    fun getInstance(): Retrofit{
+    fun getInstance(): Retrofit {
+        val httpClient = OkHttpClient.Builder()
+            .connectTimeout(CONNECT_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .addInterceptor(Interceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Content-Type", CONTENT_TYPE_JSON)
+                    .build()
+                chain.proceed(request)
+            })
+            .build()
+
         return Retrofit
             .Builder()
             .baseUrl(BASE_URL)
+            .client(httpClient)
             .addConverterFactory(
-                GsonConverterFactory.create())
+                GsonConverterFactory.create()
+            )
             .build()
     }
 }
